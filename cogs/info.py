6ebc02f5c521aa7da01920a8a31d138d20c60bdc
Soli <Soli0222@proton.me>
2023-03-29 19:00:33 +0900

    バージョン記載と変更内容の記述

--- cogs/info.py
+++ cogs/info.py
@@ -18,7 +18,7 @@ class InfoCog(commands.Cog):
     @app_commands.guilds(int(settings.getId()))
     async def hoge(self, interaction: discord.Interaction):
         dt_now = datetime.datetime.now()
-        await interaction.response.send_message("Charmy for Xanadu by Soli\nVersion: Rev 0.0.3\nUpdated: 2023/03/25\n"+dt_now.strftime('%Y/%m/%d/ %H:%M:%S'))
+        await interaction.response.send_message("Charmy for Xanadu by Soli\nVersion: Rev 0.0.4\nUpdated: 2023/03/29\n"+dt_now.strftime('%Y/%m/%d/ %H:%M:%S'))
 
 async def setup(bot: commands.Bot):
     await bot.add_cog(InfoCog(bot))
